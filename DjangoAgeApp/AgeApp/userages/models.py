5f7ca4b6d6ddd8b059bd44ff4dbc19ae1c3340a6
Eli Coyle <elijcoyle@outlook.com>
2019-09-04 14:55:10 -0400

    create model for user age and add migration

--- DjangoAgeApp/AgeApp/userages/models.py
+++ DjangoAgeApp/AgeApp/userages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class AgeProfile(models.Model):
+#     email = models.CharField(max_length=50)
+#     age = models.IntegerField()
+
+# Probably go with this one-- deleting and such. 
+# Just make sure it isn't unsafe to use a direct realationship to an internal auth class.
+class AgeProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='age_profile')
+    age = models.IntegerField()
